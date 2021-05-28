12aec030c2365f91a6147dafd32c8c44dda832e3
ProMehedi <mehedibd563@gmail.com>
2021-05-28 23:28:10 +0600

    Get Users Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([])
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const reqData = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        )
+        const resData = await reqData.json()
+        setData(resData)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUsers()
+  }, [])
+
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
